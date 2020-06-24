4531645c519ddea853d26e2c938175e218c6d2d9
manutka17 <figes@rambler.ru>
2020-06-24 20:17:36 +0300

    Тест примеры

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,14 @@
 package ru.stqa.pft.sandbox;
 
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String somebody="world";
+        System.out.println("Hello, " + somebody + "!");
+
+        Double l=7.78;
+        Double s=l*l;
+        System.out.println("Площадь квадрата"+l+"="+s);
     }
 }
