7eb9c2c70b3b180c5a46310b60d6d5e6e7a8d4ea
manutka17 <figes@rambler.ru>
2020-06-27 17:31:03 +0300

    Перенесла код в фикстуру

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateNewUserTest.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateNewUserTest.java
@@ -19,16 +19,17 @@ public class CreateNewUserTest {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testCreateNewUser() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testCreateNewUser() throws Exception {
+
     wd.findElement(By.linkText("add new")).click();
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
