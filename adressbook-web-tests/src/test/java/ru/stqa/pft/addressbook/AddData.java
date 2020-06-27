e4caeb977a3f2fdfb736e701e31fd8d0cf90c124
manutka17 <figes@rambler.ru>
2020-06-27 17:58:23 +0300

    Задание 4. Рефакторинг кода, тесты для создания нового адреса

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class AddData {
+  private final String ferstname;
+  private final String middlename;
+  private final String lastName;
+  private final String number;
+  private final String mail;
+
+  public AddData(String ferstname, String middlename, String lastName, String number, String mail) {
+    this.ferstname = ferstname;
+    this.middlename = middlename;
+    this.lastName = lastName;
+    this.number = number;
+    this.mail = mail;
+  }
+
+  public String getFerstname() {
+    return ferstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+}
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateNewUserTest.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateNewUserTest.java
@@ -19,36 +19,56 @@ public class CreateNewUserTest {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testCreateNewUser() throws Exception {
 
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Анна");
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Ивановна");
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Желябова");
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("+7955555555");
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("sgdfgsdf@dgdf.ru");
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    gotoAddNewPage();
+    fillAddPage(new AddData("Анна", "Ивановна", "Желябова", "+7955555555", "sgdfgsdf@dgdf.ru"));
+    submitAddCreation();
+    returntoAddPage();
+  }
+
+  private void returntoAddPage() {
     wd.findElement(By.linkText("home page")).click();
     wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitAddCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillAddPage(AddData addData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(addData.getFerstname());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(addData.getMiddlename());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(addData.getLastName());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(addData.getNumber());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(addData.getMail());
+  }
+
+  private void gotoAddNewPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
@@ -74,5 +94,5 @@ public class CreateNewUserTest {
     }
   }
 
-  }
+}
 
