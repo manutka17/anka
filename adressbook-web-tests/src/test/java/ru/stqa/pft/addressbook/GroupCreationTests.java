4efe07ccd52b78f71a172b5d923609b0bbbac5ef
manutka17 <figes@rambler.ru>
2020-07-01 15:57:07 +0300

    Задание 5. Реализован общий класс TestBase, в него перемещены вспомогательные методы

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,34 +1,9 @@
 package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class GroupCreationTests {
-  private WebDriver wb;
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wb = new ChromeDriver();
-    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wb.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wb.findElement(By.name("user")).clear();
-    wb.findElement(By.name("user")).sendKeys(username);
-    wb.findElement(By.name("pass")).clear();
-    wb.findElement(By.name("pass")).sendKeys(password);
-    wb.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
@@ -37,57 +12,7 @@ public class GroupCreationTests {
     fillGroupForm(new GroupData("test1", "test2", "test3"));
     submitGroupCreation();
     returntoGroupPage();
-    wb.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.linkText("Logout")).click();
   }
 
-  private void returntoGroupPage() {
-    wb.findElement(By.linkText("group page")).click();
-  }
-
-  private void submitGroupCreation() {
-    wb.findElement(By.name("submit")).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    wb.findElement(By.name("group_name")).click();
-    wb.findElement(By.name("group_name")).clear();
-    wb.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wb.findElement(By.name("group_header")).click();
-    wb.findElement(By.name("group_header")).clear();
-    wb.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wb.findElement(By.name("group_footer")).click();
-    wb.findElement(By.name("group_footer")).clear();
-    wb.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation() {
-    wb.findElement(By.name("new")).click();
-  }
-
-  private void gotoGroupPage() {
-    wb.findElement(By.linkText("groups")).click();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wb.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wb.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wb.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
