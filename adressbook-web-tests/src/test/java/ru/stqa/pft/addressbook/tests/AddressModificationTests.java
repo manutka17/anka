a393eefef7e25f64d6937b055c4ce72553bac232
manutka17 <figes@rambler.ru>
2020-07-21 21:44:32 +0300

    4.5 для групп

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressModificationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressModificationTests.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.AddData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class AddressModificationTests extends TestBase {
   @Test
@@ -12,14 +15,16 @@ public class AddressModificationTests extends TestBase {
     if (! app.getContactHelper().isTereAContact()){
       app.getContactHelper().createContact(new AddData("Анна", null, null, null, null,"test1"));
     }
-    int before=app.getContactHelper().getUserCount();
-    app.getContactHelper().initContactModification(before-1);
+    //int before=app.getContactHelper().getUserCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getContactHelper().initContactModification(before.size()-1);
     app.getContactHelper().fillAddPage(new AddData("Аннад", "Ивановна", "Гелябова", "+7955555555", "sgdfgsdf@dgdf.ru",null), false);
     app.getContactHelper().submitAddModification();
 
     app.getNavigationHelper().gotoHomePage();
-    int after=app.getContactHelper().getUserCount();
-    Assert.assertEquals(after, before);
+    //int after=app.getContactHelper().getUserCount();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
 
   }
 }
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DelitedUserTest.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DelitedUserTest.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.AddData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class DelitedUserTest extends TestBase {
 
@@ -14,14 +17,17 @@ public class DelitedUserTest extends TestBase {
       app.getContactHelper().createContact(new AddData("Анна", null, null, null, null,"test1"));
 
     }
-    int before=app.getContactHelper().getUserCount();
-    app.getContactHelper().selectFerstUser(before-1);
+    //int before=app.getContactHelper().getUserCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+
+    app.getContactHelper().selectFerstUser(before.size()-1);
     app.getContactHelper().deletedSelectUser();
 
     app.getContactHelper().closeAlert();
     app.getNavigationHelper().gotoHomePage();
-    int after=app.getContactHelper().getUserCount();
-    Assert.assertEquals(after, before-1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    //int after=app.getContactHelper().getUserCount();
+    Assert.assertEquals(after.size(), before.size()-1);
 
   }
 
