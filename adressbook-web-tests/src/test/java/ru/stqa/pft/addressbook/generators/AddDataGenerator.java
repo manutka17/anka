cfdbd02c2b2bcd893fe2759ad92f18fcf4f60404
manutka17 <figes@rambler.ru>
2020-08-02 20:38:15 +0300

    6.3

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/AddDataGenerator.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/AddDataGenerator.java
@@ -1,4 +1,7 @@
 package ru.stqa.pft.addressbook.generators;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.AddData;
 
 import java.io.File;
@@ -9,11 +12,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddDataGenerator {
-  public  static  void  main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Contact count")
+  public  int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public  String file;
+
+  public static void main(String[] args) throws IOException {
+    AddDataGenerator generator = new AddDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    }catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
     List<AddData> contacts = generateContacts(count);
-    save(contacts,file);
+    save(contacts,new File(file));
   }
 
   private static void save(List<AddData> contacts, File file) throws IOException {
@@ -28,7 +47,7 @@ public class AddDataGenerator {
   private static List<AddData> generateContacts(int count) {
     List<AddData> contacts = new ArrayList<AddData>();
     for (int i = 0; i < count; i++){
-      contacts.add(new AddData().withFerstname(String.format("test %s", i)).withLastName(String.format("test %s", i)).withEmail1(String.format("test %s", i)));
+      contacts.add(new AddData().withFerstname(String.format("FerstName %s", i)).withLastName(String.format("LastName %s", i)).withEmail1(String.format("Email1 %s", i)));
     }
     return  contacts;
   }
