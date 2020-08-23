cb4341cac10351fa005b3351593fe224093d828b
manutka17 <figes@rambler.ru>
2020-08-24 00:24:22 +0300

    vxcvxcv

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanajer/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanajer/ApplicationManager.java
@@ -33,7 +33,6 @@ public class ApplicationManager {
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     dbHelper = new DbHelper();
     if ("".equals(properties.getProperty("selenium.server"))) {
@@ -41,6 +40,7 @@ public class ApplicationManager {
         wd = new FirefoxDriver();
       } else if (browser.equals(BrowserType.CHROME)) {
         wd = new ChromeDriver();
+      }
       } else {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName(browser);
@@ -54,7 +54,7 @@ public class ApplicationManager {
       contactHelper = new ContactHelper(wd);
       sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
-  }
+  
 
     public WebDriver getFirefoxDriver () {
       return wd;
